794c89dd8dc33a984291c9917590cdad7007e882
harshaal06 <harshalaglawe06@gmail.com>
2024-01-15 15:16:21 +0530

    some changes

--- js/review.js
+++ js/review.js
@@ -40,7 +40,7 @@ function loadList() {
     for (let i = 0; i < reviewListName.length; i++) {
         let name = reviewListName[i];
         let msg = reviewListMsg[i];
-        reviewBox.innerHTML = reviewBox.innerHTML + `<div class="card p-3 col-3 shadow m-4">
+        reviewBox.innerHTML = reviewBox.innerHTML + `<div class="card p-3 col-md-3 col-xs-12 col-sm-4 shadow m-4">
         <h4 class="text-center mt-3">${name}</h4>
         <img src="../images/review-img.png" class="wdt bag p-2 d-block m-auto rounded-circle my-3">
         <p class="mt-1 m-2 text-center">${msg}</p>
